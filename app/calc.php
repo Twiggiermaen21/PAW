db0560958e29e7782ed2846ef7e33a62f5c192dd
[Kacper Pudelko] <[kacper.pudelko.kpk@gmail.com]>
2024-03-07 16:54:31 +0100

    added plugin files

---
+++ app/calc.php
@@ -0,0 +1,73 @@
+<?php
+// KONTROLER strony kalkulatora
+require_once dirname(__FILE__).'/../config.php';
+
+// W kontrolerze niczego nie wysyła się do klienta.
+// Wysłaniem odpowiedzi zajmie się odpowiedni widok.
+// Parametry do widoku przekazujemy przez zmienne.
+
+// 1. pobranie parametrów
+
+$x = $_REQUEST ['x'];
+$y = $_REQUEST ['y'];
+$operation = $_REQUEST ['op'];
+
+// 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
+
+// sprawdzenie, czy parametry zostały przekazane
+if ( ! (isset($x) && isset($y) && isset($operation))) {
+	//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+}
+
+// sprawdzenie, czy potrzebne wartości zostały przekazane
+if ( $x == "") {
+	$messages [] = 'Nie podano liczby 1';
+}
+if ( $y == "") {
+	$messages [] = 'Nie podano liczby 2';
+}
+
+//nie ma sensu walidować dalej gdy brak parametrów
+if (empty( $messages )) {
+	
+	// sprawdzenie, czy $x i $y są liczbami całkowitymi
+	if (! is_numeric( $x )) {
+		$messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
+	}
+	
+	if (! is_numeric( $y )) {
+		$messages [] = 'Druga wartość nie jest liczbą całkowitą';
+	}	
+
+}
+
+// 3. wykonaj zadanie jeśli wszystko w porządku
+
+if (empty ( $messages )) { // gdy brak błędów
+	
+	//konwersja parametrów na int
+	$x = intval($x);
+	$y = intval($y);
+	
+	//wykonanie operacji
+	switch ($operation) {
+		case 'minus' :
+			$result = $x - $y;
+			break;
+		case 'times' :
+			$result = $x * $y;
+			break;
+		case 'div' :
+			$result = $x / $y;
+			break;
+		default :
+			$result = $x + $y;
+			break;
+	}
+}
+
+// 4. Wywołanie widoku z przekazaniem zmiennych
+// - zainicjowane zmienne ($messages,$x,$y,$operation,$result)
+//   będą dostępne w dołączonym skrypcie
+include 'calc_view.php';
---
+++ app/calc_view.php
@@ -0,0 +1,45 @@
+<?php require_once dirname(__FILE__) .'/../config.php';?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+<meta charset="utf-8" />
+<title>Kalkulator</title>
+</head>
+<body>
+
+<form action="<?php print(_APP_URL);?>/app/calc.php" method="post">
+	<label for="id_x">Liczba 1: </label>
+	<input id="id_x" type="text" name="x" value="<?php print($x); ?>" /><br />
+	<label for="id_op">Operacja: </label>
+	<select name="op">
+		<option value="plus">+</option>
+		<option value="minus">-</option>
+		<option value="times">*</option>
+		<option value="div">/</option>
+	</select><br />
+	<label for="id_y">Liczba 2: </label>
+	<input id="id_y" type="text" name="y" value="<?php print($y); ?>" /><br />
+	<input type="submit" value="Oblicz" />
+</form>	
+
+<?php
+//wyświeltenie listy błędów, jeśli istnieją
+if (isset($messages)) {
+	if (count ( $messages ) > 0) {
+		echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
+		foreach ( $messages as $key => $msg ) {
+			echo '<li>'.$msg.'</li>';
+		}
+		echo '</ol>';
+	}
+}
+?>
+
+<?php if (isset($result)){ ?>
+<div style="margin: 20px; padding: 10px; border-radius: 5px; background-color: #ff0; width:300px;">
+<?php echo 'Wynik: '.$result; ?>
+</div>
+<?php } ?>
+
+</body>
+</html>
