b9be5605190096950e5c6971e850f056522b5c4d
Kacper Pudełko <115584491+Twiggiermaen21@users.noreply.github.com>
2024-03-15 14:54:33 +0100

    Add files via upload

---
+++ LAB2/app/security/check.php
@@ -0,0 +1,11 @@
+<?php
+require_once dirname(__FILE__).'/../../config.php';
+
+session_start();
+
+$role = isset($_SESSION['role']) ? $_SESSION['role'] : '';
+
+if ( empty($role) ){
+	include _ROOT_PATH.'/app/security/login.php';
+	exit();
+}
---
+++ LAB2/app/security/login.php
@@ -0,0 +1,47 @@
+<?php
+require_once dirname(__FILE__).'/../../config.php';
+
+function getParamsLogin(&$form){
+	$form['login'] = isset ($_REQUEST ['login']) ? $_REQUEST ['login'] : null;
+	$form['pass'] = isset ($_REQUEST ['pass']) ? $_REQUEST ['pass'] : null;
+}
+
+function validateLogin(&$form,&$messages){
+	if ( ! (isset($form['login']) && isset($form['pass']))) {
+		return false;
+	}
+
+	if ( $form['login'] == "") {
+		$messages [] = 'Nie podano loginu';
+	}
+	if ( $form['pass'] == "") {
+		$messages [] = 'Nie podano hasła';
+	}
+
+	if (count ( $messages ) > 0) return false;
+
+	if ($form['login'] == "admin" && $form['pass'] == "admin") {
+		session_start();
+		$_SESSION['role'] = 'admin';
+		return true;
+	}
+	if ($form['login'] == "user" && $form['pass'] == "user") {
+		session_start();
+		$_SESSION['role'] = 'user';
+		return true;
+	}
+	
+	$messages [] = 'Niepoprawny login lub hasło';
+	return false; 
+}
+
+$form = array();
+$messages = array();
+
+getParamsLogin($form);
+
+if (!validateLogin($form,$messages)) {
+	include _ROOT_PATH.'/app/security/login_view.php';
+} else { 
+	header("Location: "._APP_URL);
+}
