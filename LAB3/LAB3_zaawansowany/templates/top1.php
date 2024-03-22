c72f78557cffceb8de51a501973fc9955e883c2b
Kacper Pudełko <115584491+Twiggiermaen21@users.noreply.github.com>
2024-03-22 16:17:41 +0100

    Add files via upload

---
+++ LAB3/LAB3_zaawansowany/templates/top1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<title>Kalkulator</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link rel="stylesheet" href="<?php print(_APP_URL); ?> /assets/css/main.css" />
+	<noscript>
+		<link rel="stylesheet" href="<?php print(_APP_URL); ?> /assets/css/noscript.css" />
+	</noscript>
+</head>
+
+<body class="is-preload">
+
+	<div id="wrapper">
+
+		<nav id="nav">
+			<a href="#kredytowy" class="icon solid fa-money-bill"><span>Kredytowy</span></a>
+			<a href="#zwykly" class="icon solid fa-calculator"><span>Zwykły</span></a>
+			<a href="<?php print(_APP_ROOT); ?>/app/security/logout.php" class="icon solid">Wyloguj</a>
+		</nav>
