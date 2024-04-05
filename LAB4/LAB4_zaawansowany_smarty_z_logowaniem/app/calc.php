a78565664aac4e9db0e3e427be33d5ca378c5b75
Kacper Pudełko <115584491+Twiggiermaen21@users.noreply.github.com>
2024-04-05 13:25:27 +0200

    Update calc.php

--- LAB4/LAB4_zaawansowany_smarty_z_logowaniem/app/calc.php
+++ LAB4/LAB4_zaawansowany_smarty_z_logowaniem/app/calc.php
@@ -1,13 +1,8 @@
 <?php
-//Skrypt uruchamiający akcję wykonania obliczeń kalkulatora
-// - należy zwrócić uwagę jak znacząco jego rola uległa zmianie
-//   po wstawieniu funkcjonalności do klasy kontrolera
-
 require_once dirname(__FILE__).'/../config.php';
 include $conf->root_path.'/app/security/check.php';
-//załaduj kontroler
+
 require_once $conf->root_path.'/app/CalcCtrl.class.php';
 
-//utwórz obiekt i użyj
 $ctrl = new CalcCtrl();
 $ctrl->process();
