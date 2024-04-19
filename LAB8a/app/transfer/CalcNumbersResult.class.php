11d4a89302f5ab2c03c12fdc7af4be1640f42109
Kacper Pudełko <115584491+Twiggiermaen21@users.noreply.github.com>
2024-04-19 13:58:53 +0200

    Delete LAB8a directory

--- LAB8a/app/transfer/CalcNumbersResult.class.php
+++
@@ -1,7 +0,0 @@
-<?php namespace app\transfer;
-
-class CalcNumbersResult
-{
-	public $op_name;
-	public $result;
-}
--- LAB8a/ctrl.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'init.php';
-
-require_once 'routing.php';
-
-\core\SessionUtils::loadMessages();
-
-\core\App::getRouter()->go();
