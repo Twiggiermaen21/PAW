5b3484905614b52a3bdb1d621931507d44fe0416
Kacper Pudełko <115584491+Twiggiermaen21@users.noreply.github.com>
2024-06-03 11:34:47 +0200

    Delete LAB9 directory

--- LAB9/app/forms/LoginForm.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace app\forms;
-
-class LoginForm {
-	public $login;
-	public $pass;
-	public $role;
-}
--- LAB9/public/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once '../init.php';
-use core\App;
-include App::getConf()->root_path.App::getConf()->action_script;
